fa8b5863bf50dbbaf08a23eef87ba8678c8c79cb
tongwenqian <839035680@qq.com>
2019-07-04 16:45:42 +0800

    提交

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/js/app.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist/js/')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+            "style-loader", // 将 JS 字符串生成为 style 节点
+            "css-loader", // 将 CSS 转化成 CommonJS 模块
+            "postcss-loader",
+            "sass-loader" // 将 Sass 编译成 CSS，默认使用 Node Sass
+        ]
+    }
+    ]
+  }
+};
